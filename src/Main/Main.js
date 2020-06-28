efa250b6e79913ebccb4a627fbd515acd4d18d21
Vicky Vasilopoulou <vickyvasilopoulou@vickys-macbook.lan>
2020-06-28 15:42:45 +0100

    Add sections and logo

---
+++ src/Main/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Main.css";
+
+export default function Main() {
+    return (
+            <div className="main-container">
+                <h2>Λίγα λόγια για εμάς</h2>
+                <div className="main-container-text"></div>
+            </div>
+    );
+}
--- src/Page/Page.js
+++ src/Page/Page.js
@@ -3,12 +3,14 @@ import NavBar from "../NavBar/NavBar";
 import ServicesCards from "../Services/ServicesCards/ServicesCards";
 import Team from "../Team/Team";
 import MainText from "../MainText/MainText";
+import Main from "../Main/Main";
 
 export default function Page() {
     return (
         <>
             <NavBar />
             <MainText />
+            <Main/>
             <ServicesCards />
             <Team />
         </>
