f81e4de3c5a7c384e070749eb445bb33b7d5bb12
Vicky Vasilopoulou <vickyvasilopoulou@vickys-macbook.lan>
2020-07-05 09:31:51 +0100

    Address correction

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     return(
 
         <div className="footer-container" id="Contact">
-            <div className="footer-address">Διεύθυνση: ΑΓ. ΓΕΩΡΓΙΟΥ 35, Ν. ΙΩΝΙΑ</div>
+            <div className="footer-address">Διεύθυνση: ΑΓ. ΓΕΩΡΓΙΟΥ 32, Ν. ΙΩΝΙΑ</div>
             <div className="footer-number">Τηλ.: 210 - 46 13 385</div>
             <div className="footer-email">Email: adamas@otenet.gr</div>
         </div>
